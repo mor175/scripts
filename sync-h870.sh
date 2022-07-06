711784a1e2633cd1993421f02571af96b0fdb655
Pascal M <oslinux@free.fr>
2022-07-06 22:41:53 +0200

    new repo sync method and force load of prebuiltapks

--- sync-h870.sh
+++ sync-h870.sh
@@ -8,10 +8,15 @@ rm -rf device/lge/msm8996-common/
 rm -rf device/lge/g6-common/
 rm -rf device/lge/h870/
 
-$HOME/bin/repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+$HOME/bin/repo sync -c -n -j 4 --no-clone-bundle --no-tags && $HOME/bin/repo sync -c -l -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 ## if sync failed try this :
 ## $HOME/bin/repo sync -c -j1 --fail-fast --force-sync --no-clone-bundle --no-tags
+
+## eOS - remove prebuiltapks contents
+rm -rf prebuilts/prebuiltapks/*
+## eOS - force load of prebuiltapks
+git clone https://gitlab.e.foundation/e/os/android_prebuilts_prebuiltapks_lfs.git -b main
 
 source build/envsetup.sh
 
