c880497a4241371d0ac02bdad25ed45e2a397f50
Pascal M <oslinux@free.fr>
2022-07-20 15:42:51 +0200

    Only keep microG support

--- extras.sh
+++ extras.sh
@@ -9,9 +9,9 @@ git cherry-pick f93240a9fe3e74e252e6e69db6993bee751a3515
 cd ../../
 
 # Charging animation
-cd frameworks/base/
-git cherry-pick 16be597d1893c3865a910d8926ab2a42424534c0
-cd ../../
+# cd frameworks/base/
+# git cherry-pick 16be597d1893c3865a910d8926ab2a42424534c0
+# cd ../../
 
 # PixelProps
 # cd frameworks/base/
