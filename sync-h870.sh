51bd5cf24e91739a3be77864b6539134e24ede2d
Pascal M <oslinux@free.fr>
2022-07-25 01:53:20 +0200

    Update sync-h870.sh

--- sync-h870.sh
+++ sync-h870.sh
@@ -10,19 +10,8 @@ rm -rf device/lge/h870/
 
 $HOME/bin/repo sync -c -n -j4 --no-clone-bundle --no-tags && $HOME/bin/repo sync -c -l -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
-## as sync failed regularly...
-$HOME/bin/repo sync -c -j1 --fail-fast --force-sync --no-clone-bundle --no-tags
-
-## eOS - remove prebuiltapks contents
-rm -rf prebuilts/prebuiltapks/
-## eOS - force load of prebuiltapks
-cd prebuilts/
-git clone https://gitlab.e.foundation/e/os/android_prebuilts_prebuiltapks_lfs.git -b main
-mv android_prebuilts_prebuiltapks_lfs/ prebuiltapks/
-cd ../
-
 source build/envsetup.sh
 
 source scripts/fixes.sh
 
-rm -rf packages/apps/Updater/
+#rm -rf packages/apps/Updater/
